028b81817901d08d5f81ea507d79fc66cd60595f
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s13.42abudhabi.ae>
2023-03-26 02:28:10 +0400

    first submission

--- _printf.c
+++ _printf.c
@@ -1,71 +1,6 @@
 #include "main.h"
 
 /**
- *  getdigits - count number of digints wihin number
- * @num: number to count its digits
- * Return: number of digints in an int
- */
-int	getdigits(int num)
-{
-	int	d;
-
-	d = 0;
-	if (num == -2147483648)
-		return (10);
-	if (num < 0)
-	{
-		num *= -1;
-		return (getdigits(num) + 1);
-	}
-	if (num >= 0 && num < 10)
-		return (0);
-	if (num == 10)
-		return (1);
-	while (num != 0)
-	{
-		num /= 10;
-		d++;
-	}
-	return (d - 1);
-}
-
-/**
- *  _putunsinint_fd - check the code
- * @u: unsigned int
- * @fd: file descriptor to write the code
- * @n: traking number of printed chars
- */
-static void	_putunsinint_fd(unsigned int u, int fd, int *n)
-{
-	if (u > 9)
-	{
-		*n += 1;
-		_putunsinint_fd(u / 10, fd, n);
-	}
-	u = (u % 10) + 48;
-	write(fd, &u, 1);
-}
-
-/**
- * on_action2 - check the code
- * @i: index of where thie printing is
- * @argdefiner: flag for type to print
- * @argu: argument of va list for the flag
- * Return: number of printed chars
- */
-static int	on_action2(int *i, char *argdefiner, va_list argu)
-{
-	int	n;
-
-	n = 0;
-	if (argdefiner[*i] == 'u')
-		_putunsinint_fd(va_arg(argu, unsigned int), 1, &n);
-	else if (argdefiner[*i] == '%')
-		write(1, "%", 1);
-	return (n);
-}
-
-/**
  * on_action - check the code
  * @i: index of where thie printing is
  * @argdefiner: flag for type to print
@@ -95,8 +30,6 @@ static int	on_action(int *i, char *argdefiner, va_list argu)
 		c = va_arg(argu, int);
 		write(1, &c, 1);
 	}
-	else
-		n = on_action2(i, argdefiner, argu);
 	return (n);
 }
 
