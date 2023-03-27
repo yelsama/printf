86ac51ca7f8f5e00ffd5d2cbbb3a83820a0a83cc
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s13.42abudhabi.ae>
2023-03-27 05:37:32 +0400

    up

--- my_main.c
+++ my_main.c
@@ -4,8 +4,8 @@
 int main(void)
 {
     int n1, n2;
-    char *s;
-    s = NULL;
+    char s;
+
     n1 = _printf("%c", s);
     n2 = printf("%c", s);
    printf("Cmpare mine: %d    original: %d\n", n1, n2);
