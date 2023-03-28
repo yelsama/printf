d7209aa11885f144ad515737093e5ae2c1bd03f0
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s13.42abudhabi.ae>
2023-03-29 00:19:48 +0400

    up

--- _printf.c
+++ _printf.c
@@ -42,7 +42,7 @@ int	on_action2(int i, char *argdefiner, va_list argu)
 	else if (argdefiner[i] == 'r')
 	{
 		if (!argu)
-			n = _puts("%r");
+			returnt (_puts("%r"));
 		p = (char *)va_arg(argu, int *);
 		if (!p)
 			n = _puts("(null)");
